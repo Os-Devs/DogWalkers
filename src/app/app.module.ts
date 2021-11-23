5162243b7debdf0ee19ac53e19fc8f9aba5738c2
Diego Figueiredo <71049855+PlayReset@users.noreply.github.com>
2021-11-22 23:18:36 -0300

    Revert "add material"
    This reverts commit 51688621c32111e73c6ba8d9935cb8b8de100c2e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,9 +10,7 @@ import { HttpClientModule } from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
