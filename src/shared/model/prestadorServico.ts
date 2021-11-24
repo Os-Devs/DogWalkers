fcd8ae48bce4386b128cf6665710e115afbe1c7b
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2021-11-24 20:16:16 -0300

    Att Classes

--- src/shared/model/prestadorServico.ts
+++ src/shared/model/prestadorServico.ts
@@ -1,61 +1,12 @@
-import { Endereco } from "./endereco";
+import { Cliente } from "./cliente";
 
-export class PrestadorServico {
-  private _nome: string;
-  private _cpf: string;
-  private _dataNasc: Date;
-  private _senha: string;
+export class PrestadorServico extends Cliente {
   private _horarioDisponibilidade: string;
-  private _endereco: Endereco;
 
-  constructor(nome: string, cpf: string, nasc: Date, horario: string, senha: string,
+  constructor(nome: string, cpf: string, nasc: Date, horario: string, telefone: number, senha: string,
               rua: string, bairro: string, cep: string, numero: number) {
-    this._nome = nome;
-    this._cpf = cpf;
-    this._dataNasc = nasc;
+    super(nome, cpf, nasc, telefone, senha, rua, bairro, cep, numero);
     this._horarioDisponibilidade = horario;
-    this._senha = senha;
-    this._endereco = new Endereco(rua, bairro, cep, numero);
-  }
-
-  get getNome(): string {
-    return this._nome;
-  }
-
-  set setNome(newName: string) {
-    this._nome = newName;
-  }
-
-  get getCpf(): string {
-    return this._cpf;
-  }
-
-  set setCpf(newCpf: string) {
-    this._cpf = newCpf;
-  }
-
-  get getDataNasc(): Date {
-    return this._dataNasc
-  }
-
-  set setDataNasc(newNasc: Date) {
-    this._dataNasc = newNasc;
-  }
-
-  get getSenha(): string {
-    return this._senha;
-  }
-
-  set setSenha(newSenha: string) {
-    this._senha = newSenha;
-  }
-
-  get getEndereco(): Endereco {
-    return this._endereco;
-  }
-
-  set setEndereco(newEndereco: Endereco) {
-    this._endereco = newEndereco;
   }
 
   get getHorarioDisponibilidade(): string {
@@ -64,5 +15,18 @@ export class PrestadorServico {
 
   set setHorarioDisponibilidade(newHora: string) {
     this._horarioDisponibilidade = newHora;
+  }
+
+  toString(): string {
+    let output: string;
+
+    output = `Nome: ${super.getNome}
+CPF: ${super.getCpf}
+Idade: ${super.idade(super.getDataNasc)}
+Telefone: ${super.getTelefone}
+Endereço:
+  ${super.getEndereco.toString()}`;
+
+    return output;
   }
 }
