660e94d8e8bd5775d105c5dae3ddec5ab3579c00
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2021-11-28 23:05:50 -0300

    Parcial Back-Cadastro

--- src/app/layout/cadastro/cadastro.component.ts
+++ src/app/layout/cadastro/cadastro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Cliente } from "../../shared/model/cliente";
+import { PrestadorServico } from "../../shared/model/prestadorServico";
+import { ClienteService } from "../../shared/service/serviceCliente/cliente.service";
+import { PrestadorService } from "../../shared/service/servicePrestador/prestador.service";
 
 @Component({
   selector: 'app-cadastro',
@@ -6,11 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent implements OnInit {
+  type: number;
+  clientes = new Array<Cliente>();
+  prestadores = new Array<PrestadorServico>();
   hide = true;
-  constructor() { }
 
-  ngOnInit(): void {
+  validations: FormGroup;
 
+  constructor(private clienteService: ClienteService, private prestadorService: PrestadorService,
+              private formBuilder: FormBuilder) {
+    this.validations = formBuilder.group({
+      nome: [
+        '', Validators.required
+      ],
+      email: [
+          '', [Validators.required, Validators.email]
+      ],
+      cpf: [
+        '', [Validators.required, Validators.pattern("\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}")]
+      ],
+      dataNasc: [
+        '', Validators.required
+      ],
+      telefone: [
+        '', [Validators.required, Validators.pattern('(\\(?\\d{2}\\)?\\s)?(\\d{4,5}\\-\\d{4})')]
+      ]
+    });
   }
 
+  ngOnInit(): void {
+    this.clienteService.listarClientes().subscribe(
+      client => this.clientes = client
+    );
+    this.prestadorService.listarPrestador().subscribe(
+      prestador => this.prestadores = prestador
+    );
+  }
+
+  inserirPrestador() {
+
+  }
 }
--- src/app/layout/comentarios/comentarios.component.ts
+++ src/app/layout/comentarios/comentarios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import { Comentario } from "../../shared/model/comentario";
 import { ComentarioService } from "../../shared/service/serviceComentario/comentario.service";
 
